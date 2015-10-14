51a74bc3ed0b73da2af7bbe73699612b6c740f27
Kristy Caster <kristyacaster@gmail.com>
2015-10-13 18:59:15 -0700

    add menu items for search and filters

--- app/src/main/java/com/codepath/googleimagesearch/ImageSearchActivity.java
+++ app/src/main/java/com/codepath/googleimagesearch/ImageSearchActivity.java
@@ -1,7 +1,12 @@
 package com.codepath.googleimagesearch;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ImageSearchActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class ImageSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_image_search, menu);
+        setupSearchMenuItem(menu);
+        setupFiltersMenuItem(menu);
+        return true;
+
+    }
+
+    private void setupSearchMenuItem(Menu menu) {
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void setupFiltersMenuItem(Menu menu) {
+        MenuItem filtersItem = menu.findItem(R.id.action_filters);
+        filtersItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d("DEBUG", "filters clicked!");
+                return false;
+            }
+        });
     }
 }
